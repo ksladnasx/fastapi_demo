c1460f11198fe518be44a6f82b84b95d7d781eb8
ksldnasx <wh8261408@126.com>
2026-07-26 20:52:17 +0800

    init

---
+++ app/main.py
@@ -0,0 +1,31 @@
+import zoneinfo
+from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: str | None = None
+
+
+app = FastAPI()
+
+
+@app.get("/items/{id}")
+def read_item(id: str):
+    return {
+        "id": id,
+        "title": "Foo",
+        "timestamp": jsonable_encoder(datetime.now()),  # noqa: DTZ005
+    }
+
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
